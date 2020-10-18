ac84aa6a6bab7af12fa386b142423500010eeb20
Prahit Yaugand <prahityaugand@gmail.com>
2020-10-18 12:55:18 +0530

    My Smashing Colors project

---
+++ node/smashing/ch1/colors/index.js
@@ -0,0 +1,5 @@
+require( "colors" );
+console.log( "Smashing Prahit".rainbow );
+console.log( "Smashing Guhan".rainbow );
+console.log( "Smashing Prahit".green );
+console.log( "Smashing Guhan".red );
