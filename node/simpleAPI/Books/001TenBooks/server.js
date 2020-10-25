292c8bf2add3ad7095186935fd2ccdc6dc830744
Prahit Yaugand <prahityaugand@gmail.com>
2020-10-25 16:36:19 +0530

    My simple book API

--- node/simpleAPI/Books/001TenBooks/server.js
+++ node/simpleAPI/Books/001TenBooks/server.js
@@ -1,9 +1,37 @@
-
-
 var http = require("http");
+var redis = require("redis");
+var client=redis.createClient();
 
 myServer = http.createServer( function( req, res) {
-      res.write("Hello Guhan!!!!");
-      res.end();
+	console.log(req.url);
+	if ( req.url != "/") {
+		res.end(); 
+	} else {
+	client.keys("*", function( err, books) {
+		//res.setHeader('Content-Type', 'application/json');
+		numBooks = books.length;
+		console.log("Number of Books:" + numBooks);
+		for ( var i=0; i<numBooks; i++ ) {
+			book = books[i];
+			console.log(book);
+			client.hgetall(book, function(err, bookData) {
+				//console.log(bookData);
+				console.log(bookData.author);
 
-}).listen(3000);
+				writeData = book + "\t" +
+							bookData.author + "\t" +
+							bookData.title + "\t" + 
+							bookData.edition + "\t" +
+							bookData.publisher + "\n";
+
+				
+				res.write(writeData, function () {
+					if ( i == numBooks) {
+						res.end();
+					}
+				});
+			});
+		}
+	});
+	}
+}).listen(10000);
