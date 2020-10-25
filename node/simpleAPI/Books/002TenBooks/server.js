794fb219c3bd54bb05ac453b3e94f0d371a67462
Prahit Yaugand <prahityaugand@gmail.com>
2020-10-25 17:06:15 +0530

    Learning Express

---
+++ node/simpleAPI/Books/002TenBooks/server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function( req, res) {
+	res.write("Hello all", function() {
+		res.end();
+	});
+});
+
+app.get("/guhan", function( req, res) {
+	res.write("Hello Guhan", function() {
+		res.end();
+	});
+})
+
+
+app.get("/prahit", function( req, res) {
+	res.write("Hello Prahit", function() {
+		res.end();
+	});
+})
+
+app.listen(10000);
